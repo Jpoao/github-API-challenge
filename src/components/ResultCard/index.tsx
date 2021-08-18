58a884f3f86f3709e5072097429d5ab797e49c94
João Paulo <mg_ses@hotmail.com>
2021-08-18 01:49:12 -0300

    add: Result card, and api integration

---
+++ src/components/ResultCard/index.tsx
@@ -0,0 +1,41 @@
+import "./styles.css";
+
+type Props = {
+    avatar_url: string;
+    html_url: string;
+    followers: number;
+    location: string;
+    name: string;
+};
+
+const ResultCard = ({ avatar_url, html_url, followers, location, name }: Props) => {
+  return (
+    <div className="search_bottom_container">
+      <div className="result_container">
+        <div className="img_container">
+          <img src={avatar_url} alt="user" />
+        </div>
+        <div className="info_container">
+          <h5>Informações</h5>
+          <p>
+            Perfil:%20
+            <a className="href" href= {html_url}>
+              {html_url}
+            </a>
+          </p>
+          <p>
+            Seguidores: <a>{followers}</a>
+          </p>
+          <p>
+            Localidade: <a>{location}</a>
+          </p>
+          <p>
+            Nome: <a>{name}</a>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ResultCard;
--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,23 +1,72 @@
+/* eslint-disable jsx-a11y/anchor-has-content */
+import axios from "axios";
+import React, { useState } from "react";
+import ResultCard from "../../components/ResultCard";
 import "./styles.css";
 
+type FormData = {
+  user: string;
+};
+
+type Info = {
+  avatar_url: string;
+  html_url: string;
+  followers: number;
+  location: string;
+  name: string;
+};
+
 const Search = () => {
+  const [formData, setFormData] = useState<FormData>({
+    user: "",
+  });
+
+  const [info, setInfo] = useState<Info>();
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    axios
+      .get(`https://api.github.com/users/${formData.user}`)
+      .then((response) => {
+        setInfo(response.data);
+      })
+      .catch((error) => {
+        setInfo(undefined);
+      });
+  };
+
   return (
     <div className="search_container">
       <div className="search_top_container">
         <h1>Encontre um perfil Github</h1>
-        <input
-          type="text"
-          name="user"
-          className="search-input"
-          placeholder="Usuário Github"
-        />
-        <div className="button_container">
-          <button type="submit" className="btn btn-primary search-button">
-            Encontrar
-          </button>
-        </div>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="user"
+            className="search-input"
+            placeholder="Usuário Github"
+            value={formData.user}
+            onChange={handleChange}
+          />
+          <div className="button_container">
+            <button type="submit" className="btn btn-primary search-button">
+              Encontrar
+            </button>
+          </div>
+        </form>
       </div>
-      <div className="search_bottom_container"></div>
+        {info && (
+          <>
+            <ResultCard avatar_url = {info.avatar_url} followers={info.followers} html_url={info.html_url} location={info.location} name={info.name} />
+          </>
+        )}
     </div>
   );
 };
